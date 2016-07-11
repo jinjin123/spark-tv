196dad25583a1b8c5cffffad80922d0fca01e473
Jinjin123 <1293813551@qq.com>
2016-07-11 20:44:36 +0800

    sockets.length must < 9

--- index.js
+++ index.js
@@ -70,8 +70,10 @@ wss.on ('connection', function connection(ws) {
         console.log(sockets);
         console.log(clients);
     });
-    sockets.push(id);
-    console.log(sockets);
+    if (sockets.length < 8) {
+      sockets.push(id);
+      console.log(sockets);
+    }
 });
 server.on('request', app);
 server.listen(port, function () { console.log('Listening on ' + server.address().port);});
